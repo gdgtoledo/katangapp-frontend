fe588046a6c9db0ae00bb7fc7c2438bc98887c89
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2015-12-07 12:15:50 +0100

    remove no need comments

--- src/components/run.js
+++ src/components/run.js
@@ -3,5 +3,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-// Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
