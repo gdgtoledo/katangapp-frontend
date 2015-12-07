8742d2fc64760e7e8baf52d3b5a96d7334d3cc05
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2015-12-07 12:19:43 +0100

    remove yeoman markup image

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,6 @@ require('styles/app.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 class AppComponent extends React.Component {
   render() {
     return (
