a0ec87d9ec4041f85e7d71a4e92369dc67ef1724
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2015-12-07 21:03:09 +0100

    remove not need run.js file

--- src/components/run.js
+++
@@ -1,6 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-
-ReactDOM.render(<App />, document.getElementById('app'));
