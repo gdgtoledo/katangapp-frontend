7caa1fc645dd7aaadae99762f3b359949aeea65e
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2015-12-07 21:03:44 +0100

    change reference to app.js removing reference to run.js

--- cfg/dev.js
+++ cfg/dev.js
@@ -11,7 +11,7 @@ var config = _.merge({
   entry: [
     'webpack-dev-server/client?http://127.0.0.1:8000',
     'webpack/hot/only-dev-server',
-    './src/components/run'
+    './src/components/app'
   ],
   cache: true,
   devtool: 'eval',
