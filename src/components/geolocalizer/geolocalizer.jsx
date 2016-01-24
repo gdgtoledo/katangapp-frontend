86a4b27bf20665eb5ed0253b6ef2f9728da7e2e0
Javier López de Ancos <javierlopez@packlink.es>
2016-01-24 12:05:56 +0100

    Add events and Html5 geolocalization when user click button

--- src/components/geolocalizer/geolocalizer.jsx
+++ src/components/geolocalizer/geolocalizer.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 
 var Geolocalizer = React.createClass ({
+	getInitialState: function () {
+		return {
+			latitude: '0',
+			longitude: '0'
+		};
+	},
+  	geolocalizeMe: function () {
+  		var success = function (position) {
+  			this.setState({latitude: position.coords.latitude});
+  			this.setState({longitude: position.coords.longitude});
+  		}.bind(this);
+  		var error = function () {
+  			console.log('sorry you have and error with ' + error);
+  		}.bind(this);
+  		if (navigator.geolocation) {
+		  navigator.geolocation.getCurrentPosition(success, error);
+		} else {
+		  error('geolocalization is not supported by browser');
+		}
+  	},
   render: function () {
     return (
-      <button className="ktg-btn--geolocalizer">
-		  </button>
+      	<button type='button' className="ktg-btn--geolocalizer" onClick={this.geolocalizeMe}>
+		</button>
     );
   }
 });
