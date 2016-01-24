509251f72c5ce4542ddbbd79bdfc79ad9c73b6e6
Félix Ortega <ortegamorenofelix@gmail.com>
2016-01-25 00:01:14 +0100

    Fix error loading logo.png

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+var logo = require('../../images/logo.png');
 
 var LogoVerticalWhite = React.createClass ({
   render: function () {
     return (
     	<figure className="ktg-logo" >
-		  <img className="ktg-logo__icon" src="../../images/logo.png" />
+		  <img className="ktg-logo__icon" src={logo} />
 		  <figcaption className="ktg-logo__tipo">katanga</figcaption>
 		</figure>
     );
