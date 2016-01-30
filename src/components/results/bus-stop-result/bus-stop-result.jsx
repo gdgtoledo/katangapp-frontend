27e1d80e9b21476fbba2339ef3089c9888e4fdc4
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-01-30 14:34:02 +0100

    [#43] Define basic bus stop react component

---
+++ src/components/results/bus-stop-result/bus-stop-result.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var BusStopResult = React.createClass ({
+  render: function () {
+    return (
+    	<article>
+    		<header className="ktg-bus-stop-result__header">
+    			<h2 className="ktg-bus-stop-result__title">Bus Stop</h2>
+    		</header>
+      </article>
+    );
+  }
+});
+
+module.exports = BusStopResult;
