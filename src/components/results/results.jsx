e63ec8e054ffe006d17a6dcafb8163c4a2d42499
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-01-30 14:03:13 +0100

    [#42] Create a main results react component

---
+++ src/components/results/results.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var Results = React.createClass ({
+  render: function () {
+    return (
+    	<articles>
+    		<header className="ktg-results__header">
+    			<h1 className="ktg-results__title">Results</h1>
+    		</header>
+      </articles>
+    );
+  }
+});
+
+module.exports = Results;
