881c5147df554dc18a9f30931be132dc4cb8ee85
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-01-30 14:36:16 +0100

    [#42] Source formatting (prefer spaces over tabs)

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 var Results = React.createClass ({
   render: function () {
     return (
-    	<articles>
-    		<header className="ktg-results__header">
-    			<h1 className="ktg-results__title">Results</h1>
-    		</header>
+      <articles>
+        <header className="ktg-results__header">
+          <h1 className="ktg-results__title">Results</h1>
+        </header>
       </articles>
     );
   }
