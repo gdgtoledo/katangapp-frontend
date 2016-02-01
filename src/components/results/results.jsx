9035f3fc2fd0a66f604f9c40596b6f20e562ebed
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-02-01 07:08:54 +0100

    [#43] Main controller must include bus-stop-result (we'll iterate over a list of bus stops in the future)

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+var BusStopResult = require('./bus-stop-result/bus-stop-result');
+
 var Results = React.createClass ({
   render: function () {
     return (
@@ -7,6 +9,7 @@ var Results = React.createClass ({
         <header className="ktg-results__header">
           <h1 className="ktg-results__title">Results</h1>
         </header>
+        <BusStopResult />
       </articles>
     );
   }
