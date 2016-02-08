07729c192f4175908e9e80af28a9a3bc4953b42b
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-02-08 09:20:07 +0100

    [#52] Define basic CSS for main results component

--- src/containers/results/results.jsx
+++ src/containers/results/results.jsx
@@ -7,9 +7,9 @@ var busStopList = [];
 var Results = React.createClass ({
   render: function () {
     return (
-      <articles>
-        <header className="ktg-results__header">
-          <h1 className="ktg-results__title">Results</h1>
+      <articles className="ktg-bus-stops-results">
+        <header className="ktg-bus-stops-results__header">
+          <h1 className="ktg-bus-stops-results__title">Results</h1>
         </header>
         {busStopList.map(function(busStop, i){
           return <BusStopsList key={i} />;
