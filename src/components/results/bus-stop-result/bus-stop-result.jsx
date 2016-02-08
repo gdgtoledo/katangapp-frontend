33b8a0a70fc605045f298faa5c70485a1cfdac8b
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-02-08 09:03:43 +0100

    [#52] Improve CSS classNames for bus stop results component

--- src/components/results/bus-stop-result/bus-stop-result.jsx
+++ src/components/results/bus-stop-result/bus-stop-result.jsx
@@ -7,11 +7,11 @@ var routeResults = [];
 var BusStopResult = React.createClass ({
   render: function () {
     return (
-      <article>
-        <header className="ktg-bus-stop-result__header">
-          <h2 className="ktg-bus-stop-result__title">Bus Stop</h2>
+      <article className="ktg-bus-stop-results">
+        <header className="ktg-bus-stop-results__header">
+          <h2 className="ktg-bus-stop-results__title">Bus Stop</h2>
         </header>
-        <table id="ktg-bus-stop-result-route-results">
+        <table className="ktg-bus-stop-results__routes">
           <thead>
             <tr>
               <th colSpan="2">
