a01fa30ffce7a180bd6eca359c8da1822937892a
Javier López de Ancos <javierlopez@packlink.es>
2016-02-17 18:00:37 +0100

    add reducer to geolocalize

---
+++ src/reducers/reducers.js
@@ -0,0 +1,45 @@
+import { loadingState } from '../actions/actions'
+import { initialState } from './initialState'
+
+import fetch from 'isomorphic-fetch'
+
+//
+//  reducers helpers
+//
+
+function requestAroundMe(state, action, meters, coors) {
+  fetch('secret-depths-4660.herokuapp/paradas?lt=' + coors.latitude + '&ln=' + coors.longitude '&r=' + meters)
+    .then(function(){
+      loadingState = true
+    })
+    .then(function(response) {
+      if (response.status >= 400) {
+          Object.assign({}, state, {
+          loading: loadingState.HIDE,
+          error: {
+            type: response.status,
+            msg: response.message
+          }
+        })
+      }
+      loadingState = false
+      return Object.assign({}, state, {
+        loading: loadingState.HIDE,
+        aroundMe: response.json()
+      })
+    })
+  })
+}
+
+//
+//  reducers case by action
+//
+
+export function geolocalize(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_BUS_STOPS_AROUND_ME:
+      return requestAroundMe()
+    default:
+      return state
+  }
+}
