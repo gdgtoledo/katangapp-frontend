f86522cc9b3c3a5dc6da144edccb04aeddfe95c5
Javier López de Ancos <javierlopez@packlink.es>
2016-03-09 20:11:09 +0100

    change to ES6 and do some corrections, add here initial state

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,5 +1,12 @@
-import { createStore } from 'redux'
-import { katangapp } from '../reducers/reducers.js'
-
-export store = createStore(katangapp)
+export const initialState = {
+  busStopsAroundMe: {
+    meters: 10,
+    coors: {
+      latitude: 10,
+      longitude: 10
+    },
+    busStops: [],
+    error: {}
+  }
+}
 
