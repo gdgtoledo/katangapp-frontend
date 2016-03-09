1864932a09936189829617f552ec0f2f6b171b95
Javier López de Ancos <javierlopez@packlink.es>
2016-03-09 19:54:30 +0100

    switch to ES6 syntax and add new helpers

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,25 +1,52 @@
+import { routeActions } from 'react-router-redux';
+import fetch from 'isomorphic-fetch';
+
 //
-//  action types
+//  actions types
 //
 
 export const REQUEST_BUS_STOPS_AROUND_ME = 'REQUEST_BUS_STOPS_AROUND_ME'
 
 //
-// other constants
+//  actions helpers
 //
 
-export loadingState = false
+function requestBusStopsAroundMe( coors, meters ) {
+  fetch( 'secret-depths-4660.herokuapp/paradas?lt=' + coors.latitude + '&ln=' + coors.longitude + '&r=' + meters )
+    .then( function(){
+      dispatch( routeActions.push( '/loading' ) );
+    })
+    .then( function( response ) {
+      if ( response.status >= 400 ) {
+        dispatch( routeActions.push( '/errors' ) );
+        return {
+          meters: meters,
+          coors: coors,
+          busStops: {},
+          error: {
+            type: response.status,
+            message: response.message
+          }
+        }
+      }
+      dispatch( routeActions.push( '/results' ) );
+      return {
+        meters: meters,
+        coors: coors,
+        busStops: response.json(),
+        error: {}
+      }
+    })
+}
 
 //
-//  action creators
+//  actions creators
 //
 
-export function requestBusStopsAroundMe(meters, coors) {
+export function getBusStopsAroundMe( coors, meters ) {
+  var busStopsApiResponse = requestBusStopsAroundMe( coors, meters );
   return {
     type: REQUEST_BUS_STOPS_AROUND_ME,
-    text: 'get all the routes and bus stops around user geolocalization',
-    meters: meters,
-    latitude: coors.laitude,
-    longitude: coors.longitude
-  }
+    busStopsAroundMe: busStopsApiResponse
+  };
 }
