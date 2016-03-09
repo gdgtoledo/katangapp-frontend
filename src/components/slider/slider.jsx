d71decc64f0c2d319bda1510ed803d0cb1eb11cb
Javier López de Ancos <javierlopez@packlink.es>
2016-03-09 20:06:51 +0100

    change to ES6 syntax and fix issues

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,42 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var Slider = React.createClass ({
-  propTypes: {
+export default class Slider extends React.Component {
+
+  constructor( props ) {
+    super( props );
+  }
+
+  static defaultProps = {
+    min: '0',
+    max: '1000',
+    step: '25',
+    setMeters: ( meters ) => { this.setState( { meters: meters } ) }
+  };
+
+  static propTypes = {
     min: React.PropTypes.string,
     max: React.PropTypes.string,
     step: React.PropTypes.string,
-    meters: React.PropTypes.string.isRequired
-  },
-  getDefaultProps: function () {
-    return {
-      min: '0',
-      max: '1000',
-      step: '25'
-    }
-  },
-  getInitialState: function () {
-    return {
-      active: false
-    }
-  },
-  showMeters: function (event) {
-    this.setState({active: true});
-  },
-  hideMeters: function (event) {
-    this.setState({active: false});
-  },
-  getMeters: function () {
-    var meters = ReactDOM.findDOMNode(this.refs.metersAround).value;
-    this.props.updateMeters(meters);
-  },
-  render: function () {
+    meters: React.PropTypes.string.isRequired,
+    setMeters: React.PropTypes.func
+  };
+
+  state = {
+    active: false
+  };
+
+  showMeters = () => {
+    this.setState( { active: true } );
+  };
+
+  hideMeters = () => {
+    this.setState( { active: false } );
+  };
+
+  getMeters = () => {
+    let meters = ReactDOM.findDOMNode( this.refs.metersAround ).value;
+    this.props.setMeters( meters );
+  };
+
+  render() {
     return (
-      <div id="ktg-slider" className="ktg-slider">
+      <div id='ktg-slider' className='ktg-slider'>
         <input
-          name="metersAround"
-          ref="metersAround"
-          type="range"
+          name='metersAround'
+          ref='metersAround'
+          type='range'
           min={this.props.min}
           max={this.props.max}
           step={this.props.step}
@@ -45,15 +54,15 @@ var Slider = React.createClass ({
           onBlur={this.hideMeters}
           onChange={this.getMeters}/>
         <output
-          htmlFor="metersAround"
-          ref="showMeters"
+          htmlFor='metersAround'
+          ref='showMeters'
           className={this.state.active ? 'ktg-slider__output is-active' : 'ktg-slider__output'}>
             {this.props.meters}
-            <span className="ktg-slider__output-unit">metros</span>
+            <span className='ktg-slider__output-unit'>metros</span>
         </output>
-		  </div>
+      </div>
     );
   }
-});
 
-module.exports = Slider;
+}
+
