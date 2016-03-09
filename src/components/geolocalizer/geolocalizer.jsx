e310c45c006700cad0da9444ef4bbe1576960ffd
Javier López de Ancos <javierlopez@packlink.es>
2016-03-09 20:02:52 +0100

    change to ES6 syntax and resolve some fixed

--- src/components/geolocalizer/geolocalizer.jsx
+++ src/components/geolocalizer/geolocalizer.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 
-var Geolocalizer = React.createClass ({
-	getInitialState: function () {
-		return {
-			latitude: '0',
-			longitude: '0'
-		};
-	},
-  	geolocalizeMe: function () {
-  		var success = function (position) {
-  			this.setState({latitude: position.coords.latitude});
-  			this.setState({longitude: position.coords.longitude});
-  		}.bind(this);
-  		var error = function () {
-  			console.log('sorry you have and error with ' + error);
-  		}.bind(this);
-  		if (navigator.geolocation) {
-		    navigator.geolocation.getCurrentPosition(success, error);
-		  } else {
-		    error('geolocalization is not supported by browser');
-		  }
-  	},
-  render: function () {
+export default class Geolocalizer extends React.Component {
+
+  constructor( props ) {
+    super( props );
+  }
+
+  static defaultProps = {
+    setCoorsAroundMe: () => true
+  };
+
+  state = {
+    latitude: '0',
+    longitude: '0'
+  };
+
+  static propTypes = {
+    setCoorsAroundMe: React.PropTypes.func
+  };
+
+  geolocalizeMe = () => {
+    let success = ( position )  => {
+      this.setState( { latitude: position.coords.latitude } );
+      this.setState( { longitude: position.coords.longitude } );
+    };
+    let error = ( err ) => {
+      throw 'sorry you have and error with ' + err;
+    };
+    if ( navigator.geolocation ) {
+      navigator.geolocation.getCurrentPosition( success, error );
+    } else {
+      throw 'geolocalization is not supported by browser';
+    }
+    this.props.setCoorsAroundMe( this.state.latitude, this.state.longitude );
+  };
+
+  render() {
     return (
       <button type='button' className='ktg-btn--geolocalizer' onClick={this.geolocalizeMe}>
-		  </button>
+      </button>
     );
   }
-});
 
-module.exports = Geolocalizer;
+}
+
