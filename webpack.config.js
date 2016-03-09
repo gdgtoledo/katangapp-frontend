52fcddd62e497379e23f99d6139d742203d8ca34
Javier López de Ancos <javierlopez@packlink.es>
2016-03-09 20:11:30 +0100

    change configuration to new structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,41 @@ var path = require('path');
 var srcPath = path.join(__dirname, 'src');
 
 module.exports = {
-    entry: [
-      'webpack/hot/dev-server',
-      'webpack-dev-server/client?http://localhost:8080',
-      path.resolve(__dirname, 'src/containers/app/app.js')
-    ],
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    module: {
-      preLoaders: [{
-          test: /\.(js|jsx)$/,
-          include: srcPath,
-          loader: 'babel!eslint-loader'
-        }],
-      loaders: [
-        {
-          test: /\.css$/,
-          loader: 'style-loader!css-loader!postcss-loader'
-        },
-        {
-          test: /\.sass/,
-          loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
-        },
-        {
-          test: /\.scss/,
-          loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
-        },
-        {
-          test: /\.(png|jpg|gif|woff|woff2)$/,
-          loader: 'url-loader?limit=8192'
-        }
-      ]
-    },
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    path.resolve(__dirname, 'src/app/app.jsx')
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+  },
+  module: {
+    preLoaders: [{
+        test: /\.(js|jsx)$/,
+        include: srcPath,
+        loader: 'babel!eslint-loader'
+      }],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      },
+      {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2)$/,
+        loader: 'url-loader?limit=8192'
+      }
+    ]
+  },
 };
