9170a4a4f5b0e2462bb6cc91d62ce5cade55ad9d
Javier López de Ancos <javierlopez@packlink.es>
2016-03-09 20:10:20 +0100

    change to ES6 and do some corrections

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,55 +1,15 @@
-import { loadingState } from '../actions/actions'
-import { initialState } from './initialState'
-
-import fetch from 'isomorphic-fetch'
-
-//
-//  reducers helpers
-//
-
-function requestAroundMe(state, action, meters, coors) {
-  fetch('secret-depths-4660.herokuapp/paradas?lt=' + coors.latitude + '&ln=' + coors.longitude '&r=' + meters)
-    .then(function(){
-      loadingState = true
-    })
-    .then(function(response) {
-      if (response.status >= 400) {
-          Object.assign({}, state, {
-          loading: loadingState.HIDE,
-          error: {
-            type: response.status,
-            msg: response.message
-          }
-        })
-      }
-      loadingState = false
-      return Object.assign({}, state, {
-        loading: loadingState.HIDE,
-        aroundMe: response.json()
-      })
-    })
-  })
-}
+import { initialState } from '../stores/stores.js';
+import { REQUEST_BUS_STOPS_AROUND_ME } from '../actions/actions.js'
 
 //
 //  reducers case by action
 //
 
-function geolocalize(state = initialState, action) {
-  switch (action.type) {
+export function busStopsAroundMe( state=initialState, action ) {
+  switch ( action.type ) {
     case REQUEST_BUS_STOPS_AROUND_ME:
-      return requestAroundMe()
+      return  action.busStopsAroundMe;
     default:
-      return state
+      return state;
   }
 }
-
-//
-//  Combine reducers
-//
-
-const katangapp = combineReducers({
-  geolocalize
-})
-
-export default katangapp
