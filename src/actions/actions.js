0ad26f2e601a83732deaf63d9201bf6fa73df43b
Javier López de Ancos <javierlopez@packlink.es>
2016-03-18 08:02:35 +0100

    add new actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,52 +1,34 @@
-import { routeActions } from 'react-router-redux';
-import fetch from 'isomorphic-fetch';
-
 //
 //  actions types
 //
 
-export const REQUEST_BUS_STOPS_AROUND_ME = 'REQUEST_BUS_STOPS_AROUND_ME'
-
-//
-//  actions helpers
-//
-
-function requestBusStopsAroundMe( coors, meters ) {
-  fetch( 'secret-depths-4660.herokuapp/paradas?lt=' + coors.latitude + '&ln=' + coors.longitude + '&r=' + meters )
-    .then( function(){
-      dispatch( routeActions.push( '/loading' ) );
-    })
-    .then( function( response ) {
-      if ( response.status >= 400 ) {
-        dispatch( routeActions.push( '/errors' ) );
-        return {
-          meters: meters,
-          coors: coors,
-          busStops: {},
-          error: {
-            type: response.status,
-            message: response.message
-          }
-        }
-      }
-      dispatch( routeActions.push( '/results' ) );
-      return {
-        meters: meters,
-        coors: coors,
-        busStops: response.json(),
-        error: {}
-      }
-    })
-}
+export const GET_BUS_STOPS_AROUND_ME  = 'GET_BUS_STOPS_AROUND_ME'
+export const SET_METERS_AROUND_ME     = 'SET_METERS_AROUND_ME'
+export const SET_COORS_AROUND_ME      = 'SET_COORS_AROUND_ME'
 
 //
 //  actions creators
 //
 
-export function getBusStopsAroundMe( coors, meters ) {
-  var busStopsApiResponse = requestBusStopsAroundMe( coors, meters );
+export function setMetersAroundMe( meters ) {
   return {
-    type: REQUEST_BUS_STOPS_AROUND_ME,
-    busStopsAroundMe: busStopsApiResponse
+    type: SET_METERS_AROUND_ME,
+    meters: meters
   };
 }
+
+export function setCoorsAroundMe( coors ) {
+  return {
+    type: SET_COORS_AROUND_ME,
+    coors: coors
+  };
+}
+
+export function getBusStopsAroundMe( coors, meters ) {
+  return {
+    type: GET_BUS_STOPS_AROUND_ME,
+    meters: meters,
+    coors: coors
+  };
+}
+
