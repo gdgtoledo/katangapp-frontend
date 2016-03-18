cdd90117be456cad0f9c62e3b6182dec7c02c9cf
Javier López de Ancos <javierlopez@packlink.es>
2016-03-18 08:03:01 +0100

    new container structure changes

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -6,14 +6,12 @@ import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-//import { reduxReactRouter, routerStateReducer, ReduxRouter } from 'redux-router';
 import { routerReducer, syncHistoryWithStore } from 'react-router-redux'
 
 import * as reducers from '../reducers/reducers.js'
 
-import GetBusStopsGeoState from '../containers/getBusStopsGeoState.js';
+import GetBusStopsGeoState from '../containers/getBusStopsGeoState.js'
 
-//import { Home } from '../components/home/home.jsx'
 import Loading from '../components/loading/loading.jsx'
 import Results from '../components/results/results.jsx'
 
@@ -25,21 +23,6 @@ const katangapp = combineReducers({
 const store = createStore(katangapp)
 
 const history = syncHistoryWithStore( browserHistory, store )
-/*
-ReactDOM.render(
-  <Provider store={store}>
-    <div className='ktg-container'>
-      <Router history={history}>
-        <Route path="/" component={GetBusStopsGeoState}>
-          <IndexRoute component={GetBusStopsGeoState}/>
-          <Route path="loading" component={Loading}/>
-          <Route path="results" component={Results}/>
-        </Route>
-      </Router>
-    </div>
-  </Provider>,
-  document.getElementById('app')
-)*/
 
 export default class App extends React.Component {
   render() {
