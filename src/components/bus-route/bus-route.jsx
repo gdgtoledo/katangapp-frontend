9d290908d7e3befb121fb338f624b21c9ce2f7fc
Javier López de Ancos <javierlopez@packlink.es>
2016-07-06 14:22:11 +0200

    adapt and rename to new redux flow

--- src/components/bus-route/bus-route.jsx
+++ src/components/bus-route/bus-route.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 export default class BusRoute extends React.Component {
-  constructor(props) {
-      super(props);
-  }
-  render() {
-    return (
-      <tr className="ktg-bus-route">
-        <td className="ktg-bus-route__id">
-          L61
-        </td>
-        <td className="ktg-bus-route__time">
-          10 min
-        </td>
-      </tr>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <tr className="ktg-bus-route">
+                <td className="ktg-bus-route__id">
+                    L61
+                </td>
+                <td className="ktg-bus-route__time">
+                    10 min
+                </td>
+            </tr>
+        );
+    }
 }
