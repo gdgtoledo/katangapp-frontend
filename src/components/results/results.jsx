84a0f68b28c5124c14aa63dc01882b0872efcde0
Javier López de Ancos <javierlopez@packlink.es>
2016-07-06 14:20:30 +0200

    add component to redux page

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-//import BusStop from '../bus-stop/bus-stop';
+
+import BusRoutes from '../bus-routes/bus-routes';
 
 export default class Results extends React.Component {
-  constructor(props) {
-      super(props);
-  }
-  render() {
-    return (
-      <articles>
-        <header className="ktg-results__header">
-          <h1 className="ktg-results__title">Results</h1>
-        </header>
-        //<BusStop />
-      </articles>
-    );
-  }
-}
 
+    constructor( props ) {
+      super( props );
+    }
+
+    static propTypes = {
+        results: React.PropTypes.isRequired
+    };
+
+    render() {
+        return (
+            <main className='ktg-container--app'>
+                <header className="ktg-results__header">
+                    <h1 className="ktg-results__title">Results</h1>
+                </header>
+                <BusRoutes busStopsRoutes={this.props.results}/>
+            </main>
+        );
+    }
+
+}
