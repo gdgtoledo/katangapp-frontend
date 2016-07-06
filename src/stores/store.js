734a8e308a8518d9293016b81decbfefb1a6d429
Javier López de Ancos <javierlopez@packlink.es>
2016-07-06 14:19:54 +0200

    change way to create store with redux and redux-thunk

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,22 +1,17 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { apiMiddleware } from 'redux-api-middleware'
+import thunk from 'redux-thunk'
 import { routerReducer } from 'react-router-redux'
 
 import * as reducers from '../reducers/reducers'
-import initialState from './initialState'
 
 const reducer = combineReducers({
   ...reducers,
   routing: routerReducer
 })
 
-const createStoreWithMiddleware = applyMiddleware(apiMiddleware)(createStore);
-
-function configureStore (initialState) {
-  return createStoreWithMiddleware(reducer, initialState);
-}
-
-const store = configureStore(initialState)
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+);
 
 export default store
-
