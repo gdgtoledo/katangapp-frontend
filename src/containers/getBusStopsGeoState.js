ad7671c9ef9679b9e322f8c81b978bc31a61295c
Javier López de Ancos <javierlopez@packlink.es>
2016-07-06 14:08:07 +0200

    simplify container with redux and redux-thunk middleware

--- src/containers/getBusStopsGeoState.js
+++ src/containers/getBusStopsGeoState.js
@@ -1,67 +1,65 @@
-import { connect } from 'react-redux'
-import { browserHistory } from 'react-router'
+import { connect } from           'react-redux'
+import { browserHistory } from    'react-router'
+import Promise from               'promise-polyfill'
 
-import Home from  '../components/home/home.jsx'
-import store from '../stores/store'
+import getBusStopsAroundMe from   '../services/busStops.js'
+import Home from                  '../components/home/home.jsx'
+import store from                 '../stores/store'
+
+const goToResults = ( dispatch ) => {
+    let busStopsResults = store.getState().default
+    if ( busStopsResults.errors.length > 0 ) {
+        dispatch( browserHistory.push( '/errors' ) )
+    } else {
+        dispatch( browserHistory.push( '/results' ) )
+    }
+}
+
+const setCoordsInStore = ( dispatch, coords ) => {
+    return new Promise ( ( resolve ) => {
+        let unsuscribe = store.subscribe( () => {
+            resolve( { coords: store.getState().default.coords, meters: store.getState().default.meters } );
+            unsuscribe();
+        })
+        dispatch( { type: 'SET_COORDS_AROUND_ME', coords: coords } )
+    });
+}
+
+const setCoordsAndGetBusStops = ( coords ) => {
+    return dispatch => {
+        return setCoordsInStore( dispatch, coords ).then(
+            position => {
+                dispatch( getBusStopsAroundMe( { coords: position.coords, meters: position.meters } ) )
+                let unsuscribe = store.suscribe( () => {
+                    goToResults( dispatch );
+                    unsuscribe();
+                })
+            }
+        )
+    }
+}
 
 const mapStateToProps = ( state ) => {
-  return {
-    meters: state.default.meters,
-    coords: state.default.coords
-  }
+    return {
+        meters: state.default.meters,
+        coords: state.default.coords
+    }
 }
 
 const mapDispatchToProps = ( dispatch ) => {
-
-  return {
-
-    setMetersAroundMe: ( meters ) => {
-      dispatch( { type: 'SET_METERS_AROUND_ME', meters: meters } );
-    },
-
-    getBusStopsAroundMeAndGoToResults: ( coords ) => {
-
-      function setCoords (coords) {
-        dispatch( { type: 'SET_COORDS_AROUND_ME', coords: coords } )
-      }
-
-      function getBusStopsAroundMe ( coords, meters ) {
-        dispatch( { type: 'GET_BUS_STOPS_AROUND_ME', coords: coords, meters: meters } )
-      }
-
-      function goToResults ( busStopsResults, unsuscribeToStore ) {
-        unsuscribeToStore()
-        if ( busStopsResults.errors.length > 0 ) {
-          dispatch( browserHistory.push( '/errors' ) );
-        } else {
-          dispatch( browserHistory.push( '/results' ) );
+    return {
+        setMetersAroundMe: ( meters ) => {
+            dispatch( { type: 'SET_METERS_AROUND_ME', meters: meters } );
+        },
+        getBusStopsAroundMeAndGoToResults: ( coords ) => {
+            dispatch( setCoordsAndGetBusStops( coords ) )
         }
-      }
-
-      function suscribeToStore () {
-        return store.subscribe( () => {
-          const searchBusStopsState = store.getState().default
-          getBusStopsAroundMe( searchBusStopsState.coords, searchBusStopsState.meters );
-          goToResults( searchBusStopsState );
-        })
-      }
-
-      function init ( coords ) {
-        let unsuscribe = suscribeToStore()
-        setCoords( coords, unsuscribe )
-      }
-
-      init( coords );
-
     }
-
-  }
-
 }
 
 const GetBusStopsGeoState = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )( Home )
 
 export default GetBusStopsGeoState
