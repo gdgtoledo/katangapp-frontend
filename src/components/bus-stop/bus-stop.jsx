6d392f23c7af29a68f4a75d8702bf5e78c255155
Javier López de Ancos <javierlopez@packlink.es>
2016-07-06 14:21:48 +0200

    adapt to new redux flow

--- src/components/bus-stop/bus-stop.jsx
+++ src/components/bus-stop/bus-stop.jsx
@@ -4,31 +4,35 @@ var BusRoute = require('../bus-route/bus-route');
 
 var busRoutes = [new BusRoute, new BusRoute];
 
-var BusStop = React.createClass ({
-  render: function () {
-    return (
-      <article className="ktg-bus-stop">
-        <header className="ktg-bus-stop__header">
-          <h2 className="ktg-bus-stop__title">Bus Stop</h2>
-        </header>
-        <table className="ktg-bus-stop__routes">
-          <thead>
-            <tr>
-              <th colSpan="2">
-                  <span className="ktg-bus-stop__description">Avda. de Barber</span>
-                  <span className="ktg-bus-stop__distance">300 meters</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {busRoutes.map(function(busRoute, i){
-              return <BusRoute key={i} />;
-            })}
-          </tbody>
-        </table>
-      </article>
-    );
-  }
-});
+export default class BusStop extends React.Components {
 
-module.exports = BusStop;
+    render() {
+        return (
+            <article className="ktg-bus-stop">
+                <header className="ktg-bus-stop__header">
+                    <h2 className="ktg-bus-stop__title">Bus Stop</h2>
+                </header>
+                <table className="ktg-bus-stop__routes">
+                    <thead>
+                        <tr>
+                            <th colSpan="2">
+                                <span className="ktg-bus-stop__description">
+                                Avda. de Barber
+                                </span>
+                                <span className="ktg-bus-stop__distance">
+                                300 meters
+                                </span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {busRoutes.map(function(busRoute, i){
+                            return <BusRoute key={i} />;
+                        })}
+                    </tbody>
+                </table>
+            </article>
+        );
+    }
+
+}
