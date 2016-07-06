a41e1e207220103cd442b2bd962b27f454cb1915
Javier López de Ancos <javierlopez@packlink.es>
2016-07-06 13:29:05 +0200

    simplify reducers with new actions and split by functionality

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,43 +1,53 @@
 import initialState from '../stores/initialState'
 
-import { SET_COORDS_AROUND_ME, SET_METERS_AROUND_ME, /*GET_BUS_STOPS_AROUND_ME_REQUEST,*/ GET_BUS_STOPS_AROUND_ME_SUCCESS, GET_BUS_STOPS_AROUND_ME_ERROR } from '../actions/actions.js'
+import {
+    SET_LOADING_STATE,
+    SET_COORDS_AROUND_ME,
+    SET_METERS_AROUND_ME,
+    GET_BUS_STOPS_AROUND_ME_SUCCESS,
+    GET_BUS_STOPS_AROUND_ME_ERROR
+} from '../actions/actions.js'
 
 //
 //  reducers case by action
 //
 
-const busStopsAroundMe = ( state=initialState, action ) => {
-  let nextState
-  switch ( action.type ) {
-    case GET_BUS_STOPS_AROUND_ME_SUCCESS:
-      var busStopsResponse = action.payload;
-      nextState = Object.assign( {}, state, { busStops: busStopsResponse, errors: [] } )
-      console.log( 'GET BUSTOPS REDUCER SUCCES' )
-      console.log( nextState )
-      return nextState
-    case GET_BUS_STOPS_AROUND_ME_ERROR:
-      var errors = action.payload;
-      nextState = Object.assign( {}, state, { busStops: {}, errors: { getBusStopsAroundMe: errors } } )
-      console.log( 'GET BUSTOPS REDUCER ERROR' )
-      console.log( nextState )
-      return nextState
-    case SET_METERS_AROUND_ME:
-      nextState = Object.assign( {}, state, { meters: action.meters } );
-      console.log( 'SET METERS REDUCER' )
-      console.log( nextState )
-      return nextState
-    case SET_COORDS_AROUND_ME:
-      nextState = Object.assign( {}, state, { coords: action.coords } )
-      console.log( 'SET COORDS REDUCER' )
-      console.log( nextState )
-      return nextState
-    default:
-      nextState = Object.assign( {}, state )
-      console.log( 'INITIAL STATE' )
-      console.log( nextState )
-      return nextState
-  }
+const setLoadingState = ( state = initialState, action ) => {
+    let nextState
+    switch ( action.type ) {
+        case SET_LOADING_STATE:
+            nextState = Object.assign( {}, state, { loading: action.state } );
+            return nextState
+        default:
+            nextState = Object.assign( {}, state )
+            return nextState
+    }
 }
 
-export default busStopsAroundMe;
+const setPositionAroundMe = ( state = initialState, action ) => {
+    let nextState
+    switch ( action.type ) {
+        case SET_LOADING_STATE:
+            nextState = Object.assign( {}, state, { meters: action.meters } );
+            return nextState
+        case SET_METERS_AROUND_ME:
+            nextState = Object.assign( {}, state, { meters: action.meters } );
+            return nextState
+        case SET_COORDS_AROUND_ME:
+            nextState = Object.assign( {}, state, { coords: action.coords } )
+            return nextState
+        case GET_BUS_STOPS_AROUND_ME_SUCCESS:
+        console.log( action.busStopsAroundMe );
+            nextState = Object.assign( {}, state, { busStops: action.busStopsAroundMe.paradas, errors: [] } )
+            return nextState
+        case GET_BUS_STOPS_AROUND_ME_ERROR:
+            nextState = Object.assign( {}, state, { busStops: {}, errors: []  } )
+            return nextState
+        default:
+            nextState = Object.assign( {}, state )
+            return nextState
+    }
+}
 
+export default setLoadingState
+export default setPositionAroundMe
