45c3d884c50e15788f7c5a84efe9c32127c9feb8
Javier López de Ancos <javierlopez@packlink.es>
2016-08-04 17:54:37 +0200

    🚗  change svg loading icon

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -11,14 +11,15 @@ export default class Loading extends React.Component {
   };
 
   static propTypes = {
-    isLoading: React.PropTypes.boolean
+    isLoading: React.PropTypes.bool.isRequired
   };
 
   render() {
     return (
       <aside className={this.props.isLoading ? 'ktg-loading ktg-loading--is-loading' : 'ktg-loading'}>
-        <svg className="ktg-loading__spiner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-           <circle className="ktg-loading__path" fill="none" strokeWidth="2" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
+        <svg className="ktg-loading__spiner" width="200px" height="200px" viewBox="0 0 66 66">
+           <circle className="ktg-loading__path" fill="none" strokeWidth="1" strokeLinecap="round" cx="33" cy="33" r="30">
+           </circle>
         </svg>
       </aside>
     );
