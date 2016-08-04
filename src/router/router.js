5ec922029df361d92961ae264af377ee2840207f
Javier López de Ancos <javierlopez@packlink.es>
2016-08-04 20:57:15 +0200

    💩  remove console.log

--- src/router/router.js
+++ src/router/router.js
@@ -4,7 +4,6 @@ import store from   '../stores/store'
 
 const goToResults = ( dispatch ) => {
     let currentState = store.getState()
-    console.log(currentState);
     if ( currentState.positionAroundMe.errors.length > 0 && currentState.positionAroundMe.busStops.length > 0 ) {
         dispatch( browserHistory.push( '/errors' ) )
     } else {
