139390ab1c1772fa85543567e12d97601835c4d6
Javier López de Ancos <javierlopez@packlink.es>
2016-08-04 17:53:22 +0200

    🚗  change react to redux component syntax in home component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,49 +1,29 @@
-import React from 'react';
+import React, { PropTypes } from 'react'
 
-import LogoVerticalWhite from '../logo/logo.jsx'
+//import LogoVerticalWhite from '../logo/logo.jsx'
 import Geolocalizer from '../geolocalizer/geolocalizer.jsx'
 import Slider from '../slider/slider.jsx'
 import Loading from '../Loading/loading.jsx'
 
-export default class Home extends React.Component {
+const Home = ( { meters, coords, loading, setLoading, setMetersAroundMe, getBusStopsAroundMe} ) => (
+    <main className='ktg-home'>
+        <header className={loading ? 'ktg-home__header ktg-home__header--is-loading' : 'ktg-home__header'}>
+            <h1 className="ktg-home__title">Pagina de inicio de katanga</h1>
+        </header>
+        <form id='ktg-home_meters-around' className={loading ? 'ktg-home__meters-around ktg-home__meters-around--is-loading' : 'ktg-home__meters-around'}>
+            <Slider meters={meters} setMeters={setMetersAroundMe} />
+            <Geolocalizer coords={coords} setCoords={getBusStopsAroundMe} setLoading={setLoading}/>
+        </form>
+        <Loading isLoading={loading}/>
+    </main>
+)
 
-    constructor( props ) {
-        super( props );
-    }
-
-    static propTypes = {
-        meters: React.PropTypes.number.isRequired,
-        setMetersAroundMe: React.PropTypes.func.isRequired,
-        getBusStopsAroundMeAndGoToResults: React.PropTypes.func.isRequired
-    };
-
-    static defaultProps = {
-        meters: 130,
-        setMetersAroundMe: () => true,
-        getBusStopsAroundMeAndGoToResults: () => true
-    };
-
-    state = {
-        loading: false
-    };
-
-    setLoading = ( isLoading ) => {
-        this.setState( { loading: isLoading } );
-    };
-
-    render() {
-        return (
-            <main className='ktg-home'>
-                <header className={this.state.loading ? 'ktg-home__header ktg-home__header--is-loading' : 'ktg-home__header'}>
-                <LogoVerticalWhite />
-                </header>
-                <form id='ktg-home_metersAround' className={this.state.loading ? 'ktg-home__metersAround ktg-home__metersAround--is-loading' : 'ktg-home__metersAround'}>
-                    <Slider meters={this.props.meters} setMeters={this.props.setMetersAroundMe} />
-                    <Geolocalizer coords={this.props.coords} setCoords={this.props.getBusStopsAroundMeAndGoToResults} setLoading={this.setLoading}/>
-                </form>
-                <Loading isLoading={this.state.loading}/>
-            </main>
-        );
-    }
-
+Home.propTypes = {
+    meters: PropTypes.number.isRequired,
+    loading: PropTypes.bool.isRequired,
+    setMetersAroundMe: PropTypes.func.isRequired,
+    getBusStopsAroundMe: PropTypes.func.isRequired,
+    setLoading: PropTypes.func
 }
+
+export default Home
