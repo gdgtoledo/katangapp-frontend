dd7cb494945724055c9c70f16c6dfd222199abe5
Javier López de Ancos <javierlopez@packlink.es>
2016-08-04 18:05:01 +0200

    🚀  add init busStops service

--- src/services/busStops.js
+++ src/services/busStops.js
@@ -1,21 +1,7 @@
 import config from  '../app/config.js'
 
 let fetchBusStopsAroundMe = ( position ) => {
-    return fetch( config.api.protocol + config.api.domain + '/' + config.api.endpoints.busStops + '?lt=' + position.coords.latitude + '&ln=' + position.coords.longitude + '&r=' + position.meters );
+    return fetch( config.api.protocol + config.api.domain + '/' + config.api.endpoints.busStops + '?lt=' + position.coords.latitude + '&ln=' + position.coords.longitude + '&r=' + position.meters, { mode: 'cors' } );
 }
 
-const getBusStopsAroundMe = ( position ) => {
-    return dispatch => {
-        return fetchBusStopsAroundMe(position).then(
-            response => {
-                dispatch( { type: 'GET_BUS_STOPS_AROUND_ME_SUCCESS', busStopsAroundMe: response } )
-            },
-            error => {
-                dispatch( { type: 'GET_BUS_STOPS_AROUND_ME_ERROR', error: error } )
-                throw error
-            }
-        )
-    }
-}
-
-export default getBusStopsAroundMe
+export default fetchBusStopsAroundMe
