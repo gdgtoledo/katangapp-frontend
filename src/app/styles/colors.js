ba9781fbe5b1a08f328de4c175ab52f4a32dac31
javierlopezdeancos <javierlopezdeancos@gmail.com>
2016-11-21 09:04:21 +0100

    💄  [#33] add colors in javascript to use in svg components

---
+++ src/app/styles/colors.js
@@ -0,0 +1,20 @@
+export default {
+    primary: "#FFDA04",
+    secondary: "#000000",
+    tertiary: "#FFCD00",
+    quaternary: "#FFFFFF",
+    grey: {
+        base: "#8c8c8c",
+        light: {
+            thirtyFive: "#e5e5e5",
+            twenty: "#bfbfbf"
+        },
+        dark: {
+            five: "#5d5d5d"
+        }
+    },
+    auxiliary: {
+        primary: "#d41717",
+        secondary: "#02a740"
+    }
+}
