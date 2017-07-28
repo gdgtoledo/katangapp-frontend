a62ecf10f79ec76b12370092ace479784b8bc5ab
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-07-28 18:34:15 +0200

    [#36] añadir intro al store de la app

--- src/stores/store.js
+++ src/stores/store.js
@@ -18,19 +18,16 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { routerReducer } from 'react-router-redux'
 
+import intro from '../reducers/intro'
 import loading from '../reducers/loading'
 import positionAroundMe from '../reducers/positionAroundMe'
 
 const reducers = combineReducers({
   loading,
+  intro,
   positionAroundMe,
-  routing: routerReducer
-})
-
-// const reducer = combineReducers({
-//   ...reducers,
-//   routing: rouerReducer
-// })
+  routing: routerReducer,
+});
 
 const store = createStore(
   reducers,
