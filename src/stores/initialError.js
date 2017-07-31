c398b64ffa07b26b5bb0a8d668cc218477e20d04
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-07-31 11:28:37 +0200

    [#79] cambiar swcrafterscllm por antiguo nombre de grupo

--- src/stores/initialError.js
+++ src/stores/initialError.js
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2016-today Software Craftmanship Toledo
+ *    Copyright 2016-today Software swcraftersclm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
