d053435274aa42d019e51a2c506762c7ef62f414
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-08-04 17:53:33 +0200

    [#92] arreglar errores de lint y formato en bus-stop component

--- src/app/components/bus-stop/bus-stop.js
+++ src/app/components/bus-stop/bus-stop.js
@@ -1,4 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import BusRoute from '../bus-route/bus-route';
 import colors from '../../styles/colors';
 import IconBusStop from '../icons/busStop';
@@ -34,6 +36,9 @@ const BusStop = ({ idbs, description, meters, routes }) =>
   </article>;
 
 BusStop.propTypes = {
+  idbs: PropTypes.number,
+  description: PropTypes.string,
+  meters: PropTypes.number,
   routes: PropTypes.array,
 };
 
