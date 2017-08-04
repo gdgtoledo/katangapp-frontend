a100d54d448b8471367a2988e244bcc2c3772440
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-08-04 13:30:55 +0200

    [#45] añadir test para intro reducers

--- src/reducers/intro.test.js
+++ src/reducers/intro.test.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import { shallow, mount } from 'enzyme';
-import renderer from 'react-test-renderer'
+import React from 'react';
 
-import calculatorReducers from '../src/js/reducers/calculatorReducers'
+import { SET_INTRO_SHOWED } from '../actions/getBusStops';
+import intro from './intro';
 
-describe('>>>R E D U C E R --- Test calculatorReducers',()=>{
-  it('+++ reducer for ADD_INPUT', () => {
-    let state = {output:100}
-    state = calculatorReducers(state,{type:"ADD_INPUTS",output:500})
-    expect(state).toEqual({output:500})
-  });
-  it('+++ reducer for SUBTRACT_INPUT', () => {
-    let state = {output:100}
-    state = calculatorReducers(state,{type:"SUBTRACT_INPUTS",output:50})
-    expect(state).toEqual({output:50})
+const initialState = { state: false };
+
+describe('intro reducer for SET_INTRO_SHOWED', () => {
+  it('should be showed', () => {
+    const newState = intro(initialState, { type: SET_INTRO_SHOWED, state: true });
+    expect(newState).toEqual({ state: true });
   });
 
+  it('should be not showed', () => {
+    const newState = intro(initialState, { type: 'DEFAULT', state: false });
+    expect(newState).toEqual({ state: false });
+  });
 });
